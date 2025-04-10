a6c7abbe3a7c5835b79f0aad8ac3fbedb36c1150
Christopher Aedo <christopher.aedo.p@gmail.com>
2025-04-09 21:53:54 -0400

    seccion 6

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,5 @@
-/** @format */
 
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, reference, z } from 'astro:content';
 
 const blogCollection = defineCollection({
 	type: 'content',
@@ -14,13 +13,32 @@ const blogCollection = defineCollection({
 			// }),
 			image: image(),
 			//relacion
-			author: z.string(),
+			// author: z.string(),
+			author: reference('author'), //referencia a la coleccion de autores
 
 			//relacion
 			tags: z.array(z.string()),
+
+			//boleaan
+			isDraft: z.boolean().default(false),
+		}),
+});
+
+const authorCollection = defineCollection({
+	type: 'data',
+	schema: ({ image }) =>
+		z.object({
+			name: z.string(),
+			avatar: image(),
+			twitter: z.string(),
+			linkedIn: z.string(),
+			github: z.string(),
+			bio: z.string(),
+			subtitle: z.string(),
 		}),
 });
 
 export const collections = {
 	blog: blogCollection,
+	author: authorCollection,
 };
